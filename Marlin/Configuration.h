6fd6d2987bab5ea482894234bcdd1cbd001e627d
Przemyslaw Wegrzyn <pwegrzyn@codepainters.com>
2014-03-30 22:12:45 +0200

    Z-axis scaling configured

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -440,8 +440,10 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402,78.7402,200.0*8/3,760*1.1}  // default steps per unit for Ultimaker
-#define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 25}    // (mm/sec)
+// Steps per unit for Prusa i3
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402, 78.7402, 4000.0, 760*1.1}
+#define DEFAULT_MAX_FEEDRATE          {500, 500, 2, 25}    // (mm/sec)
+
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for Skeinforge 40+, for older versions raise them a lot.
 
 #define DEFAULT_ACCELERATION          3000    // X, Y, Z and E max acceleration in mm/s^2 for printing moves
